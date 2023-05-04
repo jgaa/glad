7f9d3302390ed8dab3ea05be965746ee742f61a2
Jarle Aase <jgaa@jgaa.com>
2023-05-04 15:23:43 +0300

    Added proper example

--- README.md
+++ README.md
@@ -23,56 +23,7 @@ asio stackless co-routines, asio stackfull co-routines, asio/C++ 20 co-routines.
 - A key can be erased at any time. 
 - Internally, the cache use sharding to allow simultaneous processing of different keys.
 
-Example: 
-```C++
-
-#include <boost/asio/co_spawn.hpp>
-#include "asio-building-bricks/SmartCache.hpp"
-
-int main(int argc, char **argv) {
-
-    // A boost io context. This is the "thread-pool" for the cache.
-    // In this example, we use only the applications main-thread.
-    boost::asio::io_context ctx;
-    
-    // Create an instance of a cache with std::string as key and value types. 
-    auto cache = jgaa::abb::make_cache<std::string, std::string>(
-    
-        // Lambda that is called by the cache when it encounters an unknown key.
-        [this](const std::string& key, auto && cb) {
-            // A normal app would return different values based on the key,
-            // for example by calling a back-end server via gRPC or REST,
-            // or lookup the value in database. 
-            // However, for this example, we always return the same value.
-            cb({} /* No error */, "Congratulations. Here This is your value");
-        }, 
-        // The asio context
-        ctx);
-     
-    // Create an asio/C++20 co-routine
-    boost::asio::co_spawn(ctx, [&]() mutable -> boost::asio::awaitable<void> {
-        // Inside the co-routine    
-        
-        // Lookup a value in the cache. This will call our lambda above,
-        // since the cache has not seen "Key1" before, and return 
-        // after `cb()` is called. 
-        auto value = co_await cache.get("Key1", boost::asio::use_awaitable)
-        
-        // Lookup another value. This will also create a round-trip to 
-        // our lambda.
-        value = co_await cache.get("Key2", boost::asio::use_awaitable)
-        
-        // Lookup the first key. Now this key one exists in the cache, and
-        // the value is returend immediately.
-        value = co_await cache.get("Key1", boost::asio::use_awaitable)
-
-    }, boost::asio::detached);
-    
-    // Let asio run the async code above in the applications thread.
-    // When the thread reach the end of the co-routine, `run()` will return
-    // and our application end.
-    ctx.run();
-}
-```
+Examples:
+- [Simple example with C++ 20 coroutine](examples/cxx20-simple.cpp)
 
 
---
+++ examples/cxx20-simple.cpp
@@ -0,0 +1,69 @@
+
+#include <iostream>
+#include "asio-building-bricks/SmartCache.hpp"
+#include <boost/asio/co_spawn.hpp>
+
+int main(int argc, char **argv) {
+
+  // A boost io context. This is the "thread-pool" for the cache.
+  // In this example, we use only the applications main-thread.
+  boost::asio::io_context ctx;
+
+  // Create an instance of a cache with std::string as key and value types.
+  auto cache = jgaa::abb::make_cache<std::string, std::string>(
+
+      // Lambda that is called by the cache when it encounters an unknown key.
+      [](const std::string &key, auto &&cb) {
+
+          static const std::unordered_map<std::string, std::string> database = {
+            {"Key1", "Dogs"},
+            {"Key2", "Cats"}
+          };
+
+          std::clog << "Looking up key " << key << " in the database." << std::endl;
+          cb({} /* no error*/, database.at(key));
+      },
+      // The asio context
+      ctx);
+
+  // Create an asio/C++20 co-routine
+  boost::asio::co_spawn(
+      ctx,
+      [&]() mutable -> boost::asio::awaitable<void> {
+        // Inside the co-routine
+
+        // Lookup a value in the cache. This will call our lambda above,
+        // since the cache has not seen "Key1" before, and return
+        // after `cb()` is called.
+        auto value = co_await cache.get("Key1", boost::asio::use_awaitable);
+        std::clog << "Got value: " << value << std::endl;
+
+        // Lookup another value. This will also create a round-trip to
+        // our lambda.
+        value = co_await cache.get("Key2", boost::asio::use_awaitable);
+        std::clog << "Got value: " << value << std::endl;
+
+        // Lookup the first key. Now this key one exists in the cache,
+        // and the value is returend immediately.
+        value = co_await cache.get("Key1", boost::asio::use_awaitable);
+        std::clog << "Got value: " << value << std::endl;
+
+        // Let's see what heppens if we query for a key that don't exist in
+        // our "database"
+        try {
+            value = co_await cache.get("unknown-key", boost::asio::use_awaitable);
+            std::clog << "Got value: " << value << std::endl;
+        } catch(const std::exception& ex) {
+            std::clog << "Lookup failed with exception: " << ex.what() << std::endl;
+        }
+      },
+      boost::asio::detached);
+
+  // Let asio run the async code above in the applications thread.
+  // When the thread reach the end of the co-routine, `run()` will return
+  // and our application end.
+
+  std::clog << "Starting \"thread-pool\"..." << std::endl;
+  ctx.run();
+  std::clog << "\"Thread-pool\" ended. Bye." << std::endl;
+}
